e06079f77f4324fc77b1beab52fc0063e9b59382
Aman Bansal <amanbansal81228@gmail.com>
2026-01-05 11:52:27 +0530

    Fix ESLint errors blocking Netlify build

--- src/Details.js
+++ src/Details.js
@@ -17,7 +17,7 @@ import github from "./assets/techstack/github.png";
 import git from "./assets/techstack/git.png";
 import npm from "./assets/techstack/npm.png";
 import postman from "./assets/techstack/postman.png";
-import figma from "./assets/techstack/figma.png";
+// import figma from "./assets/techstack/figma.png";
 // Porject Images
 import projectImage1 from "./assets/projects/project1.jpg";
 import projectImage2 from "./assets/projects/project2.jpg";
